d9be0010c1c8f70b5a75997560c229af3fc7f3a3
Edidiong Bassey <edidiong.bassey@frogasia.com>
2022-01-05 02:37:56 +0800

    feat: Create grid view for countries display

---
+++ src/components/Countrygrid.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  makeStyles,
+  createStyles,
+  CardActionArea,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(2.5),
+    },
+  })
+);
+
+const CountryGrid = ({ data }) => {
+  const classes = useStyles();
+
+  return (
+    <Grid container spacing={2} className={classes.root}>
+      {data.map((item, i) => {
+        const { name, region, area, flag } = item;
+        return (
+          <Grid item xs={12} sm={4} md={3} key={i}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={flag}
+                  alt="flag"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {name}
+                  </Typography>
+                  <Typography color="text.secondary" variant="body2">
+                    Region: {region}
+                  </Typography>
+                  <Typography color="text.secondary" variant="body2">
+                    Area Size: {area} km²
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default CountryGrid;
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,6 @@ export { default as Countries } from "./Countries";
 export { default as Search } from "./Search";
 export { default as Sort } from "./Sort";
 export { default as CountryList } from "./Countrylist";
+export { default as CountryGrid } from "./Countrygrid";
 export { default as Pagination } from "./Pagination";
+export { default as ViewToggle } from "./Viewtoggle";
