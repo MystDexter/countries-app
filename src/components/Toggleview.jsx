c52eefd7056d608f86d08389b436f966634bd47f
Edidiong Bassey <edidiong.bassey@frogasia.com>
2022-01-05 03:32:25 +0800

    feat: Create ToggleView component for switching between list and grid view

---
+++ src/components/Toggleview.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
+import { Icon } from "@material-ui/core";
+
+const ToggleView = ({ onToggle }) => {
+  const [alignment, setAlignment] = useState("list");
+
+  const handleAlignment = (e, newAlignment) => {
+    console.log(newAlignment);
+    setAlignment(newAlignment);
+    onToggle(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+    >
+      <ToggleButton value="list" aria-label="list">
+        <Icon>view_list</Icon>
+      </ToggleButton>
+      <ToggleButton value="grid" aria-label="grid">
+        <Icon>view_module</Icon>
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
+
+export default ToggleView;
