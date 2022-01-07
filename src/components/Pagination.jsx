710c95cb8ea07e26dacb8e1353e59afd347c680d
Edidiong Bassey <edidiong.bassey@frogasia.com>
2022-01-07 22:27:31 +0800

    chore: Refactor pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
-import { makeStyles, createStyles, Button } from "@material-ui/core";
+import {
+  makeStyles,
+  createStyles,
+  Button,
+  ButtonGroup,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -10,24 +15,11 @@ const useStyles = makeStyles((theme) =>
       justifyContent: "center",
       padding: `${theme.spacing(1)}px 0 ${theme.spacing(2)}px 0`,
     },
-    paginationItem: {
-      padding: theme.spacing(2),
-      borderRadius: "50%",
-      margin: `0 ${theme.spacing(0.25)}px`,
-      color:
-        theme.palette.type == "dark" ? "inherit" : theme.palette.primary.main,
-    },
     paginationItemActive: {
-      padding: theme.spacing(2),
-      borderRadius: "50%",
-      margin: `0 ${theme.spacing(0.25)}px`,
       pointerEvents: "none",
       color: theme.palette.type == "dark" ? "black" : "white",
       backgroundColor:
         theme.palette.type == "dark" ? "white" : theme.palette.primary.main,
-    },
-    direction: {
-      margin: `0 ${theme.spacing(1)}px`,
     },
   })
 );
@@ -74,61 +66,49 @@ function Pagination({
     return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
   };
 
-  const pageButtons = () => {
-    return getPaginationGroup().map((item, index) => (
-      <Fragment key={index}>
-        {item <= pages ? (
-          <Button
-            onClick={handleChangePage}
-            variant={currentPage === item ? "contained" : "text"}
-            color="primary"
-            className={
-              currentPage === item
-                ? classes.paginationItemActive
-                : classes.paginationItem
-            }
-            style={{ maxWidth: 16, maxHeight: 16, minWidth: 16, minHeight: 16 }}
-          >
-            {item <= pages ? item : ""}
-          </Button>
-        ) : (
-          ""
-        )}
-      </Fragment>
-    ));
-  };
-
   return (
     <Fragment>
       <RenderComponent data={getPaginatedData()} />
       <div className={classes.pagination}>
-        <Button
-          onClick={goToPreviousPage}
-          className={classes.direction}
-          variant="outlined"
-          size="small"
-          disabled={currentPage === 1 ? true : false}
-        >
-          prev
-        </Button>
+        <ButtonGroup variant="outlined">
+          {/* <Button>«</Button> */}
+          <Button
+            onClick={goToPreviousPage}
+            className={classes.direction}
+            disabled={currentPage === 1 ? true : false}
+          >
+            Prev
+            {/* ‹ */}
+          </Button>
 
-        {pageButtons()}
+          {getPaginationGroup().map((item, index) => (
+            <Button
+              onClick={handleChangePage}
+              className={
+                currentPage === item ? classes.paginationItemActive : null
+              }
+              style={{ display: item <= pages ? "inherit" : "none" }}
+            >
+              {item <= pages ? item : ""}
+            </Button>
+          ))}
 
-        <Button
-          onClick={goToNextPage}
-          className={classes.direction}
-          variant="outlined"
-          disabled={
-            currentPage === pages
-              ? true
-              : data.length / dataLimit <= currentPage
-              ? true
-              : false
-          }
-          size="small"
-        >
-          next
-        </Button>
+          <Button
+            onClick={goToNextPage}
+            className={classes.direction}
+            disabled={
+              currentPage === pages
+                ? true
+                : data.length / dataLimit <= currentPage
+                ? true
+                : false
+            }
+          >
+            Next
+            {/* › */}
+          </Button>
+          {/* <Button>»</Button> */}
+        </ButtonGroup>
       </div>
     </Fragment>
   );
